a020d3ff1fdcd9b17e9862b654f816f620378fb3
Patrick Dunn <pmdunn78@gmail.com>
2020-01-21 18:12:39 -0500

    echo test

--- contact.php
+++ contact.php
@@ -4,8 +4,9 @@
 
     // youtube tutorial: https://www.youtube.com/watch?v=Ge_IZlC4N4o
         $name = $_POST["name"];
-        $email = $_POST["name"];
-        $message = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+        echo $name
 
         $email_from = "Portfolio Site";
         $email_subject = "New Message from Portfolio";
